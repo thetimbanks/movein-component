76ba2c27b2f43c6e73283225b01b486502fdddfd
Tim Banks <thetimbanks@gmail.com>
2022-04-04 18:17:10 -0500

    Add basic units table and remove default cra info

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import logo from './logo.svg';
+import UnitsTable from './UnitsTable';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </header>
+      <UnitsTable />
     </div>
   );
 }
---
+++ src/UnitsTable/index.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import styles from './styles.scss'
+
+function App() {
+  return (
+    <div className="UnitsTable">
+      <section>
+        <div className="row">
+          <div className="col unitInfo">
+            <div>
+              13.5m²  Self Storage
+            </div>
+            <div>
+              What can it fit?
+            </div>
+            <div>
+              Can Fit Storage Up to 3 Bedrooms Worth
+            </div>
+          </div>
+          <div className="col unitAmenities">
+            <ul>
+              <li>24/7 CCTV Security</li>
+              <li>Unique pin access to entry & exit to gates, lifts and storage space</li>
+              <li>24 Hour Access</li>
+              <li>Free move in trailers, vans and trucks</li>
+              <li>No lock-in contracts</li>
+              <li>Ground Unit</li>
+            </ul>
+          </div>
+          <div className="col unitPricing">
+            <div>$199.99</div>
+            <Button>Reserve</Button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col unitInfo">
+            <div>
+              13.5m²  Self Storage
+            </div>
+            <div>
+              What can it fit?
+            </div>
+            <div>
+              Can Fit Storage Up to 3 Bedrooms Worth
+            </div>
+          </div>
+          <div className="col unitAmenities">
+            <ul>
+              <li>24/7 CCTV Security</li>
+              <li>Unique pin access to entry & exit to gates, lifts and storage space</li>
+              <li>24 Hour Access</li>
+              <li>Free move in trailers, vans and trucks</li>
+              <li>No lock-in contracts</li>
+              <li>Ground Unit</li>
+            </ul>
+          </div>
+          <div className="col unitPricing">
+            <div>$199.99</div>
+            <Button>Reserve</Button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col unitInfo">
+            <div>
+              13.5m²  Self Storage
+            </div>
+            <div>
+              What can it fit?
+            </div>
+            <div>
+              Can Fit Storage Up to 3 Bedrooms Worth
+            </div>
+          </div>
+          <div className="col unitAmenities">
+            <ul>
+              <li>24/7 CCTV Security</li>
+              <li>Unique pin access to entry & exit to gates, lifts and storage space</li>
+              <li>24 Hour Access</li>
+              <li>Free move in trailers, vans and trucks</li>
+              <li>No lock-in contracts</li>
+              <li>Ground Unit</li>
+            </ul>
+          </div>
+          <div className="col unitPricing">
+            <div>$199.99</div>
+            <Button>Reserve</Button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col unitInfo">
+            <div>
+              13.5m²  Self Storage
+            </div>
+            <div>
+              What can it fit?
+            </div>
+            <div>
+              Can Fit Storage Up to 3 Bedrooms Worth
+            </div>
+          </div>
+          <div className="col unitAmenities">
+            <ul>
+              <li>24/7 CCTV Security</li>
+              <li>Unique pin access to entry & exit to gates, lifts and storage space</li>
+              <li>24 Hour Access</li>
+              <li>Free move in trailers, vans and trucks</li>
+              <li>No lock-in contracts</li>
+              <li>Ground Unit</li>
+            </ul>
+          </div>
+          <div className="col unitPricing">
+            <div>$199.99</div>
+            <Button>Reserve</Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
